9b8bf03ae960023601797ac2c9c938dcb3bc2f0e
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-06 11:59:45 +0600

    dfaf

--- src/app/Redux/store.ts
+++ src/app/Redux/store.ts
@@ -1,9 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { quizSlice } from "./feature/quiz/quizSlice";
+import { quizSlice } from "../feature/quiz/quizSlice";
 
 export const store = configureStore({
   reducer: {
     Quiz: quizSlice.reducer,
   },
 });
-
