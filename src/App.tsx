8510f3d8c35b3676eee38bcb64f730542e8e2856
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-23 22:55:24 +0600

    af

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import Question from "./home/Question";
 import QuizSummary from "./home/QuizSummary";
 
 function App() {
-  const { completeQuiz } = UseAppSelector((state) => state.quiz);
+  const { quizComplete } = UseAppSelector((state) => state.quiz);
   return (
     <div>
       <h1 className="text-center text-6xl m-8">Quiz App</h1>
-      {!completeQuiz ? <Question /> : <QuizSummary/>}
+      {!quizComplete ? <Question /> : <QuizSummary/>}
     </div>
   );
 }
