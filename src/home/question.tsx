0299a42c712344509164c7fdf0f23d1ef9f38f47
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-23 13:00:33 +0600

    asf

--- src/home/question.tsx
+++ src/home/question.tsx
@@ -1,4 +1,3 @@
- 
 import { UseAppSelector } from "@/app/Redux/hooks";
 import { Button } from "@/components/ui/button";
 import {
@@ -11,23 +10,28 @@ import {
 } from "@/components/ui/card";
 
 export default function Question() {
-  const {question,currentQuestionIndex}  = UseAppSelector((state)=>state.quiz)
-  const currentQuestion = question[currentQuestionIndex]
+  const { question, currentQuestionIndex } = UseAppSelector(
+    (state) => state.quiz
+  );
+  const currentQuestion = question[currentQuestionIndex];
   console.log(currentQuestion);
   return (
-    <div>
+    <div className="ml-12">
       <Card className="w-[350px]">
         <CardHeader>
           <CardTitle>{currentQuestion.question}</CardTitle>
           <CardDescription>
-         Question  {currentQuestionIndex +1}
+            Question {currentQuestionIndex + 1} of {question.length}
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <h1>Card Content </h1>
+          {currentQuestion.options.map((option, index) => (
+            <Button key={index} className="w-full mt-3">{option}</Button>
+          ))}
         </CardContent>
         <CardFooter className="flex justify-between">
-          <Button>Exit</Button>
+          <Button>Previous</Button>
+          <Button>Next</Button>
         </CardFooter>
       </Card>
     </div>
