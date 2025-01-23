0a078c5e851e476cc5c76e74c5d99c840318cc9b
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-23 23:00:48 +0600

    adf

--- src/home/QuizSummary.tsx
+++ src/home/QuizSummary.tsx
@@ -1,18 +1,20 @@
 import { UseAppSelector } from "@/app/Redux/hooks";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import React from "react";
+import { Progress } from "@/components/ui/progress"
 
 export default function QuizSummary() {
   const {question,userAnswers} = UseAppSelector((state)=> state.quiz)
   const correctAnswer = question.reduce((count,qna, index)=>{
     return qna.correctAnswer=== userAnswers[index] ? count+1 :count
   },0)
-  console.log(correctAnswer);
+  const correctAnswerPercentence = parseFloat((correctAnswer/question.length)*100)
   return (
     <div className="flex justify-center">
       <Card className="w-[450px]   ">
         <CardHeader className="text-center">Quiz Summary</CardHeader>
         <CardContent>You Have Got {correctAnswer} out of {question.length}</CardContent>
+        <Progress value={40}/>
       </Card>
     </div>
   );
