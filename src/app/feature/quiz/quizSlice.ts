98fc50f4b22c7557a3ce93dd55460d2f24efe2b6
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-23 12:51:21 +0600

    fsd

--- src/app/feature/quiz/quizSlice.ts
+++ src/app/feature/quiz/quizSlice.ts
@@ -1,10 +1,23 @@
-
+import { quizData } from "@/home/quizData";
 import { createSlice } from "@reduxjs/toolkit";
 
+interface TQuiz {
+  question: typeof quizData;
+  currentQuestionIndex: number;
+  useAnswers: (string | null)[];
+  quizComplete: boolean;
+}
+
+const initialState: TQuiz = {
+  question: quizData,
+  currentQuestionIndex: 0,
+  useAnswers: Array(quizData.length).fill(null),
+  quizComplete: false,
+};
 
 export const quizSlice = createSlice({
   name: "quiz",
-  initialState:{},
+  initialState,
   reducers: {},
 });
 
