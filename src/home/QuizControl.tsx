b8e2d0e0c320939953aa56893cd2c25de89c2f92
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-23 18:37:42 +0600

    ad

--- src/home/QuizControl.tsx
+++ src/home/QuizControl.tsx
@@ -1,9 +1,12 @@
 import { nextQuestion, previousQuestion } from "@/app/feature/quiz/quizSlice";
-import { useAppDispatch } from "@/app/Redux/hooks";
+import { useAppDispatch, UseAppSelector } from "@/app/Redux/hooks";
 import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function QuizControl() {
+  const {currentQuestionIndex} = UseAppSelector(
+    (state)=>state.quiz
+  )
   const dispatch = useAppDispatch();
   const handleNextQuestion = () => {
     dispatch(nextQuestion());
@@ -14,7 +17,7 @@ export default function QuizControl() {
 
   return (
     <div className="flex justify-between mt-4  ">
-      <Button onClick={handlePreviousQuestion}>Previous</Button>
+      <Button disabled={currentQuestionIndex===0} onClick={handlePreviousQuestion}>Previous</Button>
       <Button onClick={handleNextQuestion}>Next</Button>
     </div>
   );
