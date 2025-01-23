ba29fb1c2c2465ce2b450cf6d00ace27273233f2
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-23 18:18:18 +0600

    ad

--- src/home/QuizControl.tsx
+++ src/home/QuizControl.tsx
@@ -1,23 +1,21 @@
-import { nextQuestion, previousQuestion } from '@/app/feature/quiz/quizSlice'
-import { useAppDispatch } from '@/app/Redux/hooks'
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { nextQuestion, previousQuestion } from "@/app/feature/quiz/quizSlice";
+import { useAppDispatch } from "@/app/Redux/hooks";
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 export default function QuizControl() {
-  const dispatch = useAppDispatch()
-  const handleNextQuestion = () =>{
-    dispatch(nextQuestion())
-  }
-  const handlePreviosQuestion = () =>{
-    dispatch(previousQuestion())
-  }
-
-
+  const dispatch = useAppDispatch();
+  const handleNextQuestion = () => {
+    dispatch(nextQuestion());
+  };
+  const handlePreviousQuestion = () => {
+    dispatch(previousQuestion());
+  };
 
   return (
-    <div className='flex justify-between mt-4  '>
-      <Button onClick={handlePreviosQuestion}>Previous</Button>
+    <div className="flex justify-between mt-4  ">
+      <Button onClick={handlePreviousQuestion}>Previous</Button>
       <Button onClick={handleNextQuestion}>Next</Button>
     </div>
-  )
+  );
 }
